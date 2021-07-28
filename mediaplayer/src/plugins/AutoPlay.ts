c57d823390396cc14eea3fb7627cd2beac287b90
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2021-07-28 15:22:37 -0500

    Primer commit, se esta inicializando el proyecto

---
+++ mediaplayer/src/plugins/AutoPlay.ts
@@ -0,0 +1,13 @@
+import MediaPlayer from '../MediaPlayer.js';
+
+class AutoPlay {
+  constructor() { }
+  run(player: MediaPlayer) {
+    if (!player.media.muted) {
+      player.media.muted = true;
+    }
+    player.play();
+  }
+}
+
+export default AutoPlay;
